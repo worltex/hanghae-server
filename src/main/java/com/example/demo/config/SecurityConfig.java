fda72e4e6b433940673182a87e980d35299a81f6
doggai10 <doggai77@gmail.com>
2024-04-06 17:56:42 +0900

    edd : swagger& security config

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)// H2 콘솔 사용을 위한 설정
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/", "/swagger-ui/**", "/v3/**").permitAll()	// requestMatchers의 인자로 전달된 url은 모두에게 허용
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()	// H2 콘솔 접속은 모두에게 허용
+                                .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )	// 세션을 사용하지 않으므로 STATELESS 설정
+                .build();
+    }
+}
